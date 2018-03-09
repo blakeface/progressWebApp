b451202322e0e2bb5fab5128bf19829137d9aa99
netzebandito <blake.netzeband@gmail.com>
2018-03-08 18:16:48 -0700

    created form. added skeleton cdn

---
+++ README.md
@@ -0,0 +1,15 @@
+# Progressive Web App
+
+The point of this is to build a simple pwa that handles:
+* a simple page load (1 html, 1 js, & 1 css file)
+* a form submit (saved with IndexDB)
+* a api call that affects the DOM
+
+Essentially follows [Google's "Your First Progressive Web App" tutorial](https://codelabs.developers.google.com/codelabs/your-first-pwapp/#0)
+
+Run with parcel, but do what you want.
+
+## Tech
+* [ParcelJS](https://parceljs.org/)
+* IndexDB
+* Service Workers
--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 import fetchImg from './fetchImage.js'
 
 // register service worker (serviceWorker isn't available everywhere... 🍎)
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && false) {
 	navigator.serviceWorker.register('./serviceWorker.js')
 		.then(() => console.log('in index.js: service worker installed'))
 		.catch(err => console.error('🚨 in index.js:', err))
-}
-// no service workers, just load image into DOM
-else {
-	fetchImg.then(imgJSON => {
-		document.querySelector('#potatoGif').src = imgJSON.data.image_url
-	});
 }
